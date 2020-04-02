a88a27600da262676c24ff35cf2e5ce83ad93e33
professorsilvioroxo <mentorredes@gmail.com>
2020-04-01 20:24:13 -0400

    Adicionado parametros.sh

---
+++ parametros.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Trabalhando com parâmetros
+
+echo "O primeiro parametro eh $1 e o decimo eh ${10}"
+echo "O nome do programa eh $0"
+echo "foram inseridos $# parametros"
+echo "Status de exec eh $?"
+echo "parametros digitados $*"
+
